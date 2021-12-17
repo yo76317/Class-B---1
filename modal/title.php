603dbc454bb1acd13ee5489cf1d84c43ab614988
yo76317 <yo76317@gmail.com>
2021-12-17 16:35:37 +0800

    下課尚未完成

---
+++ modal/title.php
@@ -0,0 +1,13 @@
+<h3>新增標題圖片</h3>
+<hr>
+<form action="" method="post" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td>標題區圖片:</td>
+            <td> type file ,name img</td>
+        </tr>
+        <tr>
+            <td>標題區替代文字:</td>
+            <td> type text ,name text</td>
+        </tr>
+    </table>
