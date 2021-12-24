71c09246811729c361dba588a0ff52f20b3b1780
yo76317 <yo76317@gmail.com>
2021-12-24 11:40:59 +0800

    動畫圖片輪播功能

--- front/main.php
+++ front/main.php
@@ -3,28 +3,53 @@
 	<?php include "marquee.php";?>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
+
+	<div style="width:100%; padding:2px; height:290px;">
+        <div id="mwww" loop="true" style="width:100%; height:100%;">
+            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+        </div>
+    </div>
+
     <script>
+		// var宣告lin = new Array 成陣列，但沒內容所以自己生
         var lin=new Array();
+		// 陣列，但沒內容所以自己生
+		// lin[0]='圖片檔名.jpg'
+		<?php
+			//sh是1的取出
+			$mvs=$Mvim->all(['sh'=>1]);
+			foreach($mvs as $mv){
+				?>	
+				// push 把值塞在最後位補上
+				$lin.push( '<?= "img/{$mv['img']}"; ?>')
+	<?php
+	}
+	?>
+
 		var now=0;
-		if(lin.length>1)
-		{
+		// 陣列長度>0 最少為2
+		// 兩個以上的話sI間隔時間3s後執行ww(),第一次執行時now=1
+		// 因為三秒後執行..ww(),now先超車執行,時間到再執行now=1
+		// js特性
+		if(lin.length>1){
 			setInterval("ww()",3000);
 			now=1;
 		}
-		function ww()
-		{
-			$("#mwww").html("<embed loop=true src='"+lin[now]+"' style='width:99%; height:100%;'></embed>")
+		// 宣告function = jqs.  .html是選擇器
+		// loop循環,lin[now]這是陣列放now值=0
+
+		function ww(){
+			$("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
 			//$("#mwww").attr("src",lin[now])
+			// 假設length=5 , 1=>now++=2=>小餘5不執行
+			// 2++=3 => 小餘不執行
+			// 跳到 4++=5 => 等於5執行 => now=0
+			// 陣列索引值超過4以後變為0，再重頭來就會一直循環
 			now++;
 			if(now>=lin.length)
 			now=0;
 		}
     </script>
-    <div style="width:100%; padding:2px; height:290px;">
-        <div id="mwww" loop="true" style="width:100%; height:100%;">
-                <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
-        </div>
-    </div>
     
 	<div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區</span>
